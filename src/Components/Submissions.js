a595f4650b356ebb69c596cf3540851c461f3213
Chillale Sai Ganesh <125571245+sai1306@users.noreply.github.com>
2024-05-02 13:03:06 +0530

    Update Submissions.js

--- src/Components/Submissions.js
+++ src/Components/Submissions.js
@@ -1,14 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Data from './Data';
 function Submissions(id){
-    const [submissions, setSubmissions] = useState([])
     var arr = Object.values(localStorage);
     arr.shift();
      for(let i = 0;i<arr.length;i++){
         arr[i] = JSON.parse(arr[i]);
     }
-    console.log(arr);
-    // arr.map((value, index)=>console.log(value))
     return(
         <>
         <div className='container'>
